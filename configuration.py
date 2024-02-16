96f6376560fccaa33a0cee64a23a8d3e02cde1c4
LenaKlin <156857120+LenaKlin@users.noreply.github.com>
2024-02-16 23:01:00 +0300

    Add files via upload

---
+++ configuration.py
@@ -0,0 +1,4 @@
+URL_SERVICE = "https://9cf32ed9-cf85-4526-bc8e-f9548546be37.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+CREATE_ORDERS_PATH = "/api/v1/orders"
+GET_TRACK_PATH = ("/api/v1/orders/track")
